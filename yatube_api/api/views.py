a12c165ff44674d66072a11f456587958ba947cd
Nikita Maika <117031788+KaMp232@users.noreply.github.com>
2026-06-29 21:16:48 +0300

    Update views.py

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,4 @@
-from rest_framework import viewsets, permissions
-from rest_framework.response import Response
+from rest_framework import viewsets
 from django.shortcuts import get_object_or_404
 
 from posts.models import Post, Group, Comment
@@ -10,32 +9,27 @@ from .permissions import IsAuthorOrReadOnly
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (IsAuthorOrReadOnly,)
+    permission_classes = [IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
-        # Автор автоматически подставляется как текущий пользователь
         serializer.save(author=self.request.user)
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
-    """Только чтение для групп"""
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = (permissions.IsAuthenticated,)
 
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
-    permission_classes = (IsAuthorOrReadOnly,)
+    permission_classes = [IsAuthorOrReadOnly]
 
     def get_queryset(self):
-        # Возвращаем комментарии только для конкретного поста
         post_id = self.kwargs.get('post_id')
         post = get_object_or_404(Post, pk=post_id)
         return Comment.objects.filter(post=post)
 
     def perform_create(self, serializer):
-        # При создании привязываем к посту и текущему пользователю
         post_id = self.kwargs.get('post_id')
         post = get_object_or_404(Post, pk=post_id)
         serializer.save(
