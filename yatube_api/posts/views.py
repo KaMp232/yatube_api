0f62994922bb53992e8a785dea0b677b765b2e3c
Nikita Maika <117031788+KaMp232@users.noreply.github.com>
2026-06-29 22:05:25 +0300

    Update views.py

---
+++ yatube_api/posts/views.py
@@ -0,0 +1,7 @@
+# Было:
+from django.shortcuts import render  # УДАЛИТЬ
+
+# Оставляем только нужные импорты
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+# и т.д.
