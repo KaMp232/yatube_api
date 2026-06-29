0ce199f5d9e0aa8ff521b45bde5853c393347fd0
Nikita Maika <117031788+KaMp232@users.noreply.github.com>
2026-06-29 20:01:29 +0300

    Create urls.py

---
+++ yatube_api/api/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views as auth_views
+from . import views
+
+router = DefaultRouter()
+router.register('posts', views.PostViewSet, basename='posts')
+router.register('groups', views.GroupViewSet, basename='groups')
+
+urlpatterns = [
+    path('api-token-auth/', auth_views.obtain_auth_token, name='api_token_auth'),
+    path('', include(router.urls)),
+    
+    path('posts/<int:post_id>/comments/', 
+         views.CommentViewSet.as_view({'get': 'list', 'post': 'create'}),
+         name='post-comments'),
+    
+    path('posts/<int:post_id>/comments/<int:pk>/',
+         views.CommentViewSet.as_view({
+             'get': 'retrieve',
+             'put': 'update',
+             'patch': 'partial_update',
+             'delete': 'destroy'
+         }),
+         name='post-comment-detail'),
+]
