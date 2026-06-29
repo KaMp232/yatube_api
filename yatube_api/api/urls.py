e06356c330bad64b6d065115938d9433c70add4d
Nikita Maika <117031788+KaMp232@users.noreply.github.com>
2026-06-29 21:39:06 +0300

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,21 +1,25 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
-from .views import PostViewSet, GroupViewSet, CommentViewSet
+from .views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
 
 app_name = 'api'
 
 router = DefaultRouter()
 router.register('posts', PostViewSet, basename='posts')
 router.register('groups', GroupViewSet, basename='groups')
+router.register('follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
-    path('v1/api-token-auth/', views.obtain_auth_token),
+    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('v1/', include(router.urls)),
 ]
 
-# Эндпоинты для комментариев (nested routes)
 urlpatterns += [
     path(
         'v1/posts/<int:post_id>/comments/',
