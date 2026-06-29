e68da6be30002faad874a5e2daa80dd2c4c2e591
Nikita Maika <117031788+KaMp232@users.noreply.github.com>
2026-06-29 20:31:31 +0300

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -8,19 +8,23 @@ router.register('posts', views.PostViewSet, basename='posts')
 router.register('groups', views.GroupViewSet, basename='groups')
 
 urlpatterns = [
-    path('v1/api-token-auth/', auth_views.obtain_auth_token, name='api_token_auth'),
+    path('v1/api-token-auth/',
+         auth_views.obtain_auth_token,
+         name='api_token_auth'),
     path('v1/', include(router.urls)),
-
-    path('v1/posts/<int:post_id>/comments/',
-         views.CommentViewSet.as_view({'get': 'list', 'post': 'create'}),
-         name='post-comments'),
-
-    path('v1/posts/<int:post_id>/comments/<int:pk>/',
-         views.CommentViewSet.as_view({
-             'get': 'retrieve',
-             'put': 'update',
-             'patch': 'partial_update',
-             'delete': 'destroy'
-         }),
-         name='post-comment-detail'),
+    path(
+        'v1/posts/<int:post_id>/comments/',
+        views.CommentViewSet.as_view({'get': 'list', 'post': 'create'}),
+        name='post-comments'
+    ),
+    path(
+        'v1/posts/<int:post_id>/comments/<int:pk>/',
+        views.CommentViewSet.as_view({
+            'get': 'retrieve',
+            'put': 'update',
+            'patch': 'partial_update',
+            'delete': 'destroy'
+        }),
+        name='post-comment-detail'
+    ),
 ]
