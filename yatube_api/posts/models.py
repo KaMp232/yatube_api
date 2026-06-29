da7f5bd5bdd7649144647bfca6dce13ffe965ce4
Nikita Maika <117031788+KaMp232@users.noreply.github.com>
2026-06-29 21:37:35 +0300

    Update models.py

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -44,3 +44,25 @@ class Comment(models.Model):
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True
     )
+    class Follow(models.Model):
+    user = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+        related_name='follower'
+    )
+    following = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+        related_name='following'
+    )
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'following'],
+                name='unique_follow'
+            )
+        ]
+
+    def __str__(self):
+        return f'{self.user} подписан на {self.following}'
