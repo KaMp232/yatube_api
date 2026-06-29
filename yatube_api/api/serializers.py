b2957e2387df295538e079b301786bbe8c534d12
Nikita Maika <117031788+KaMp232@users.noreply.github.com>
2026-06-29 20:06:41 +0300

    Update serializers.py

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -4,26 +4,31 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class PostSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         slug_field='username',
         read_only=True
     )
+
     class Meta:
         model = Post
         fields = ('id', 'text', 'author', 'image', 'group', 'pub_date')
         read_only_fields = ('author', 'pub_date')
+
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = ('id', 'title', 'slug', 'description')
 
+
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
         slug_field='username',
         read_only=True
     )
+
     class Meta:
         model = Comment
         fields = ('id', 'author', 'post', 'text', 'created')
