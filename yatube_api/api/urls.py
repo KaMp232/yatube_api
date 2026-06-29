6dea4436e0dd0b048f4df61b6c81f2862c3d6a1e
Nikita Maika <117031788+KaMp232@users.noreply.github.com>
2026-06-29 20:06:28 +0300

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -10,11 +10,11 @@ router.register('groups', views.GroupViewSet, basename='groups')
 urlpatterns = [
     path('api-token-auth/', auth_views.obtain_auth_token, name='api_token_auth'),
     path('', include(router.urls)),
-    
-    path('posts/<int:post_id>/comments/', 
+
+    path('posts/<int:post_id>/comments/',
          views.CommentViewSet.as_view({'get': 'list', 'post': 'create'}),
          name='post-comments'),
-    
+
     path('posts/<int:post_id>/comments/<int:pk>/',
          views.CommentViewSet.as_view({
              'get': 'retrieve',
