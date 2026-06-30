6a9001e97e22686b3cad3a42c3adeee3ba72dcb6
Nikita Maika <117031788+KaMp232@users.noreply.github.com>
2026-06-30 10:17:07 +0300

    Update urls.py

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -15,7 +15,7 @@ router.register('groups', GroupViewSet, basename='groups')
 router.register('follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
-    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/token/',TokenObtainPairView.as_view(),name='token_obtain_pair'),
     path('v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('v1/', include(router.urls)),
 ]
